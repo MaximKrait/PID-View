e9a2a779b5416856e315d724b24f03e18fc29984
Maxim Krait <100289164+MaximKrait@users.noreply.github.com>
2026-06-08 02:36:25 +0400

    Added and refactored: display of threads and UIDs, new header

--- src/PID_view.c
+++ src/PID_view.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <pwd.h>
 
 struct process_in_ram {
     char name[16];
@@ -12,10 +14,14 @@ struct process_in_ram {
     char state;
     pid_t pid;
     int ppid;
+    int threads;
+    char uid_name[32];
 };
 
 void parse_proc_status(const char *buffer, struct process_in_ram *proc) {
     const char *line = buffer;
+    int raw_uid = -1;
+
     while (*line) {
         if (strncmp(line, "Name:", 5) == 0) {
             sscanf(line, "Name:\t%15s", proc->name);
@@ -23,10 +29,25 @@ void parse_proc_status(const char *buffer, struct process_in_ram *proc) {
             sscanf(line, "State:\t%c", &proc->state);
         } else if (strncmp(line, "PPid:", 5) == 0) {
             sscanf(line, "PPid:\t%d", &proc->ppid);
+        } else if (strncmp(line, "Threads:", 8) == 0) {
+            sscanf(line, "Threads:\t%d", &proc->threads);
+        } else if (strncmp(line, "Uid:", 4) == 0) {
+            sscanf(line, "Uid:\t%d", &raw_uid);
         }
 
         while (*line && *line != '\n') line++;
         if (*line == '\n') line++;
+    }
+
+    if (raw_uid != -1) {
+        struct passwd *pw = getpwuid(raw_uid);
+        if (pw) {
+            strncpy(proc->uid_name, pw->pw_name, sizeof(proc->uid_name) - 1);
+        } else {
+            snprintf(proc->uid_name, sizeof(proc->uid_name), "%d", raw_uid);
+        }
+    } else {
+        strcpy(proc->uid_name, "unknown");
     }
 }
 
@@ -60,8 +81,11 @@ int main(int argv, char *args[]) {
 
     parse_proc_status(buffer, &process);
 
-    printf("Name process: %s\n", process.name);
-    printf("State: %c\n", process.state);
+    printf("=== PID-View v0.4 ===\n");
+    printf("PID:          %d\n", process.pid);
+    printf("Name:         %s\n", process.name);
+    printf("Owner:        %s\n", process.uid_name);
+    printf("State:        %c\n", process.state);
 
     errno = 0;
     process.priority = getpriority(PRIO_PROCESS, process.pid);
@@ -70,11 +94,12 @@ int main(int argv, char *args[]) {
         return 1;
     }
 
-    printf("priority process: %d\n", process.priority);
+    printf("Priority:     %d\n", process.priority);
     if (process.ppid != 0) {
-        printf("PPid process: %d\n", process.ppid);
+        printf("Parent PID:   %d\n", process.ppid);
     }
-    printf("Program version 0.4\n");
+    printf("Threads:      %d\n", process.threads);
+    printf("=====================\n");
     
     return 0;
 }
